20bdd026a0f0f3f57cf7a71a94e829caf67ae0b0
sxrhd <758194367@qq.com>
2018-01-30 14:03:00 +0800

    Delete remove_package

--- mt6755.mk
+++ mt6755.mk
@@ -24,10 +24,6 @@ DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 # Common product elements
 include $(LOCAL_PATH)/product/*.mk
 
-# Remove packages that do not work well
-PRODUCT_PACKAGES += \
-    RemovePackages
-
 # Product common configurations
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
